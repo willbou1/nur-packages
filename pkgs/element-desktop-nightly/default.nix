41aced48f0d4c300c12c8cde7fe743c02c48e5a1
William Boulanger <willbou2@gmail.com>
2026-06-05 22:46:21 -0400

    Upgrade

--- pkgs/element-desktop-nightly/default.nix
+++ pkgs/element-desktop-nightly/default.nix
@@ -6,7 +6,7 @@ alsa-lib, atk, cairo, cups, curl, dbus, expat, fontconfig, freetype, gdk-pixbuf,
 # https://aur.archlinux.org/packages/element-desktop-nightly-bin
 ################################################################################
 let
-    version = "2026010401";
+    version = "2026060501";
 
     rpath = lib.makeLibraryPath [
         alsa-lib
@@ -66,12 +66,12 @@ let
     src = if stdenv.hostPlatform.system == "x86_64-linux" then
         fetchurl {
             url = "https://packages.element.io/debian/pool/main/e/element-nightly/element-nightly_${version}_amd64.deb";
-           sha256 = "1791af10e1a9a7ea2b9b8456c39a89e52be010118e776e17e7ee5e05d3e252bc"; 
+           sha256 = "2fa39a94043fd1131cceddbb93eb9a8119b1d333e138d9b9e14c80902b41d021"; 
         }
     else if stdenv.hostPlatform.system == "aarch64-linux" then
         fetchurl {
             url = "https://packages.element.io/debian/pool/main/e/element-nightly/element-nightly_${version}_arm64.deb";
-           sha256 = "3e31f7bbebc690151665a9b1baf6e3f9f5940077d7f2cfdb6e20433ac59c4250"; 
+           sha256 = "b5f15bed7f4b506a4e83b56e951cf9b16ab8897d41a0dae5d15c7ac5e13259cd"; 
         }
     else
         throw "element-desktop-nightly is not supported on ${stdenv.hostPlatform.system}";
